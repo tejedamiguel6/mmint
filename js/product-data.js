cfaa8e13c99a717dd62a1e3430bf027b56e37228
tejedamiguel6 <tejedamiguel6@gmail.com>
2025-07-04 15:58:04 -0600

    update

--- js/product-data.js
+++ js/product-data.js
@@ -37,33 +37,31 @@ window.addEventListener("load", () => {
 paste this function in the console for demo
 */
 
-function extractProductData() {
-  const products = document.querySelectorAll(".product-card");
-  console.log("---<>", products);
+// function extractProductData() {
+//   const products = document.querySelectorAll(".product-card");
+//   console.log("--->", products);
 
-  const results = [];
+//   const results = [];
 
-  products.forEach((product) => {
-    const stockConatiner = product.querySelector(".product-stock");
-    const stockIndicator = stockConatiner?.querySelectorAll("span");
-    const productPrices = product.querySelector(".product-price");
-    const priceSpans = productPrices?.querySelectorAll("span");
-    results.push({
-      image: product.querySelector(".product-image")?.src,
-      cardTitle: product.querySelector(".product-title")?.innerText,
-      badge: product.querySelector(".badge")?.textContent,
-      pricePrefix: productPrices?.querySelector(".price-prefix")?.textContent,
-      originalPrice: priceSpans?.[2]?.textContent,
-      reducedPrice: priceSpans?.[1]?.textContent,
-      stockIndicator:
-        stockIndicator[0]?.classList[1] || stockIndicator[0]?.classList[0],
-      stockIndicatorAmount:
-        stockIndicator[0]?.nextElementSibling?.nextSibling?.data,
-    });
-  });
-  console.log("actual results:", results);
-}
+//   products.forEach((product) => {
+//     const stockConatiner = product.querySelector(".product-stock");
+//     const stockIndicator = stockConatiner?.querySelectorAll("span");
+//     const productPrices = product.querySelector(".product-price");
+//     const priceSpans = productPrices?.querySelectorAll("span");
+//     results.push({
+//       image: product.querySelector(".product-image")?.src,
+//       cardTitle: product.querySelector(".product-title")?.innerText,
+//       badge: product.querySelector(".badge")?.textContent,
+//       pricePrefix: productPrices?.querySelector(".price-prefix")?.textContent,
+//       originalPrice: priceSpans?.[2]?.textContent,
+//       reducedPrice: priceSpans?.[1]?.textContent,
+//       stockIndicator:
+//         stockIndicator[0]?.classList[1] || stockIndicator[0]?.classList[0],
+//       stockIndicatorAmount:
+//         stockIndicator[0]?.nextElementSibling?.nextSibling?.data,
+//     });
+//   });
+//   console.log("actual results:", results);
+// }
 
-extractProductData();
-
-// setTimeout(extractProductData, 10000);
+// extractProductData();
